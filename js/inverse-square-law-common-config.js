5668255086dd967ce36c3c67ad3f06e18e2355e2
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-03-08 14:08:21 -0700

    add config file

---
+++ js/inverse-square-law-common-config.js
@@ -0,0 +1,41 @@
+// Copyright 2017, University of Colorado Boulder
+
+// RequireJS configuration file for Shred (UI component lib) demo.
+require.config( {
+  deps: [ 'sun-main' ],
+
+  paths: {
+
+    // third party libs
+    text: '../../sherpa/lib/text-2.0.12',
+
+    // plugins
+    image: '../../chipper/js/requirejs-plugins/image',
+    mipmap: '../../chipper/js/requirejs-plugins/mipmap',
+    string: '../../chipper/js/requirejs-plugins/string',
+    ifphetio: '../../chipper/js/requirejs-plugins/ifphetio',
+
+    // PhET libs, uppercase names to identify them in require.js imports
+    AXON: '../../axon/js',
+    BRAND: '../../brand/' + phet.chipper.brand + '/js',
+    DOT: '../../dot/js',
+    INVERSE_SQUARE_LAW_COMMON: '.'
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHETCOMMON: '../../phetcommon/js',
+    REPOSITORY: '..',
+    PHET_CORE: '../../phet-core/js',
+    PHET_IO: '../../phet-io/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SHERPA: '../../sherpa',
+    SUN: '../../sun/js',
+    TANDEM: '../../tandem/js',
+    VEGAS: '../../vegas/js',
+    SHRED: '../../shred/js',
+    VIBE: '../../vibe/js'
+  },
+
+  // optional cache buster to make browser refresh load all included scripts, can be disabled with ?cacheBuster=false
+  urlArgs: phet.chipper.getCacheBusterArgs()
+} );
