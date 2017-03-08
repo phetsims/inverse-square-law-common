32ccdc824bd4eb48fb575e9f368fbdaed2e4bbe1
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-03-07 17:36:36 -0700

    Add the namespace

---
+++ js/forceLabCommon.js
@@ -0,0 +1,15 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * Creates the namespace for all things under force-lab-common.
+ *
+ * @author Jesse Greenberg (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var Namespace = require( 'PHET_CORE/Namespace' );
+
+  return new Namespace( 'forceLabCommon' );
+} );
