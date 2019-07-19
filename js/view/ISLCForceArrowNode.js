00f2f516ede093636a54e6522a7cd8701c931dbb
phet-dev <phetadmin@gmail.com>
2019-07-19 03:16:56 -0600

    update copyright dates from daily grunt work

--- js/view/ISLCForceArrowNode.js
+++ js/view/ISLCForceArrowNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2018, University of Colorado Boulder
+// Copyright 2017-2019, University of Colorado Boulder
 
 /**
  * Arrow node for sims that use inverse-square-law-common.  The arrow is scaled to represent the magnitude of the force,
