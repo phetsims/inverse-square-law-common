48e29a3debc803dd14f8b0d17380348d0fb75249
phet-dev <phetadmin@gmail.com>
2020-01-13 03:29:12 -0700

    update copyright dates from daily grunt work

--- js/ISLCConstants.js
+++ js/ISLCConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Shared constants used in various places in gravity-force-lab, gravity-force-lab-basics, and coulombs-law.
