b9530298c31fbde2ea428a32307afae9954836a4
phet-dev <phetadmin@gmail.com>
2021-05-25 15:52:52 -0600

    update copyright dates from daily grunt work

--- js/view/ISLCDragBoundsNode.js
+++ js/view/ISLCDragBoundsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * Shows the boundaries of the draggable area for an ISLCObjec's center point. Adjusts with the radius of the object.
