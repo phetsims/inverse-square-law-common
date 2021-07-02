4b0b8c1c8af0f2469d2fa50d159e29d2d39657bb
Jesse <jesse.greenberg@colorado.edu>
2021-07-02 13:58:49 -0400

    Temporary query params for a survey, see https://github.com/phetsims/gravity-force-lab-basics/issues/281

--- js/ISLCQueryParameters.js
+++ js/ISLCQueryParameters.js
@@ -24,7 +24,18 @@ const ISLCQueryParameters = QueryStringMachine.getAll( {
 
   // Shows boundary positions of the two objects, as .  The boundary positions for each
   // object will change depending on the size and position of both objects.
-  showDragBounds: { type: 'flag' }
+  showDragBounds: { type: 'flag' },
+
+  // Controls which responses are initially enabled with Voicing. These are only temporary and used to support
+  // a survey that will go out about Voicing, but can be removed shortly thereafter
+  voicingNameResponsesInitiallyEnabled: { type: 'boolean', defaultValue: true },
+  voicingObjectResponsesInitiallyEnabled: { type: 'boolean', defaultValue: true },
+  voicingContextResponsesInitiallyEnabled: { type: 'boolean', defaultValue: true },
+  voicingHintResponsesInitiallyEnabled: { type: 'boolean', defaultValue: false },
+
+  // For the voicing survey mentioned above, will have modifications needed for the
+  // initial survey
+  voicingSurvey: { type: 'boolean', defaultValue: false }
 } );
 
 inverseSquareLawCommon.register( 'ISLCQueryParameters', ISLCQueryParameters );
