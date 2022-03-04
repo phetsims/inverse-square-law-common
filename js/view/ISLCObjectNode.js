eeaa7020252c520ea3d2285078baf96fbc1aef7e
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:07:36 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/view/ISLCObjectNode.js
+++ js/view/ISLCObjectNode.js
@@ -13,7 +13,7 @@
 
 import Range from '../../../dot/js/Range.js';
 import Utils from '../../../dot/js/Utils.js';
-import Shape from '../../../kite/js/Shape.js';
+import { Shape } from '../../../kite/js/imports.js';
 import merge from '../../../phet-core/js/merge.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
 import { Circle, Color, DragListener, Node, Path, RichText } from '../../../scenery/js/imports.js';
--- js/view/ISLCPullerNode.js
+++ js/view/ISLCPullerNode.js
@@ -12,7 +12,7 @@ import Bounds2 from '../../../dot/js/Bounds2.js';
 import LinearFunction from '../../../dot/js/LinearFunction.js';
 import Utils from '../../../dot/js/Utils.js';
 import Vector2 from '../../../dot/js/Vector2.js';
-import Shape from '../../../kite/js/Shape.js';
+import { Shape } from '../../../kite/js/imports.js';
 import merge from '../../../phet-core/js/merge.js';
 import { Circle } from '../../../scenery/js/imports.js';
 import { Image } from '../../../scenery/js/imports.js';
