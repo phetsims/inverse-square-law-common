51e4000ca232f15958a5e2e3ff2ee571f7bba3e3
phet-dev <phetadmin@gmail.com>
2024-06-07 03:15:51 -0600

    update copyright dates from daily grunt work

--- js/view/ISLCObjectNode.js
+++ js/view/ISLCObjectNode.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2023, University of Colorado Boulder
+// Copyright 2017-2024, University of Colorado Boulder
 
 /**
  * A common type for object views in sims that use inverse-square-law-common. The objects have a shaded sphere with a
